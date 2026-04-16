08d106b3ce7ccdcb59a886b68ed1fe84f8d5e41f
Wasim30596 <wasim3096k@gmail.com>
2026-04-16 16:22:54 +0530

    backend config commented

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -5,12 +5,12 @@ terraform {
       version = "6.41.0"
     }
   }
-  backend "s3" {
-    bucket = "my-terraform-state-bucket-1604"
-    key    = "dev/terraform.tfstate"
-    region = "ap-southeast-1"
-    dynamodb_table = "terraform-lock-table"
-  }
+  # backend "s3" {
+  #   bucket = "my-terraform-state-bucket-1604"
+  #   key    = "dev/terraform.tfstate"
+  #   region = "ap-southeast-1"
+  #   dynamodb_table = "terraform-lock-table"
+  # }
 }
 
 provider "aws" {
