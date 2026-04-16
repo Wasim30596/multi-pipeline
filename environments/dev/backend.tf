ce679a9a12ad41f6a1741de9185949d40e38be08
Wasim30596 <wasim3096k@gmail.com>
2026-04-16 15:27:20 +0530

    Added dev folder to dev branch

---
+++ environments/dev/backend.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.41.0"
+    }
+  }
+  backend "s3" {
+    bucket = "my-terraform-state-bucket-1604"
+    key    = "dev/terraform.tfstate"
+    region = "ap-southeast-1"
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
---
+++ environments/dev/main.tf
@@ -0,0 +1,15 @@
+ module "vpc" {
+   source = "../../modules/vpc"
+   vpc_cidr_block = "10.0.0.0/16"
+   subnet_cidr_block = "10.0.0.0/24"
+   env = "dev"
+ }
+
+ module "ec2" {
+   source = "../../modules/ec2"
+   instance_type = "t3.small"
+   ami_id = "ami-0e7ff22101b84bcff" # ubuntu AMI  
+   subnet_id = module.vpc.subnetid
+   instance_count =   1
+   env = "dev"
+ }
