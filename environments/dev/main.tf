40275926ab676fb6bc07c181ce4976e0c70d8e86
Wasim30596 <wasim3096k@gmail.com>
2026-04-16 16:30:21 +0530

    subnet_id is changed

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -9,7 +9,7 @@
    source = "../../modules/ec2"
    instance_type = "t3.small"
    ami_id = "ami-0e7ff22101b84bcff" # ubuntu AMI  
-   subnet_id = module.vpc.subnetid
+   subnet_id = module.vpc.subnet_id
    instance_count =   1
    env = "dev"
  }
